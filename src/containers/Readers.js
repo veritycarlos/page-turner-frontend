22495101caeb5c6d167593926c293e865163b25a
Verity Collins <verity.carlos@gmail.com>
2022-10-12 12:39:34 -0500

    added code to readers link

--- src/containers/Readers.js
+++ src/containers/Readers.js
@@ -1,12 +1,26 @@
-import React from "react"
-
+import React, { useState, useEffect } from 'react'
+import ReaderLink  from '../components/ReaderLink'
 
 const Readers = () => {
+    const [readers, setReaders] = useState([])
+    const [readerFormFlag, setReaderFormFlag] = useState(false)
 
+    useEffect(() => {
+        fetch('http://localhost:9292/readers')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setReaders(data)
+        })
+    }, [])
+
+    const readersList = readers.map( r => <ReaderLink key={r.id} reader={r} />)
 
     return (
         <div>
-            <p>Readers</p>
+            <ul>
+                {readersList}
+            </ul>
         </div>
     )
 }
