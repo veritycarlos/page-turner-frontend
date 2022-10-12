a2f5d7687300bad8f9a61cbb237427b0f442e389
Verity Collins <verity.carlos@gmail.com>
2022-10-12 12:40:17 -0500

    updated app file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-
-
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import Readers from './containers/Readers';
+import Reader from './containers/Readers';
 
 function App() {
   return (
-
+    <Router>
+      <Navigation />
       <div className="App">
-        <p>Home</p>
+        <Routes>
+          <Route exact path="/" element= {<Home/>} />
+          <Route exact path="/readers" element= {<Readers/>} />
+          <Route path="/readers/:id" element= {<Reader/>} />
+        </Routes>
       </div>
-    
+    </Router>
   );
 }
 
