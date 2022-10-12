a3181c76152ca7e5df40ab53f75f34ba66ecd2f8
Verity Collins <verity.carlos@gmail.com>
2022-10-12 12:38:47 -0500

    added Reader code

--- src/containers/Reader.js
+++ src/containers/Reader.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import Book from '../components/Book'
 
+const Reader = (props) => {
+    const [reader, setReader] = useState({
+        books: []
+    })
+    const [bookFormFlag, setBookFormFlag] = useState(false)
 
-const Reader  = () => {
-
+    const params = useParams();
+    // const readerid = useId()
+   
+    useEffect(() => {
+        debugger
+        fetch(`http://localhost:9292/readers/${params.id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }, [])
 
     return (
         <div>
-            <h3>Readerrr</h3>
+            <h3>I am a Reader</h3>
         </div>
     )
 }
