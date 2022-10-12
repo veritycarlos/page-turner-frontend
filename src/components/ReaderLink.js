70afae99ea2d2a67bbd96fa58e473fc19d6b9c02
Verity Collins <verity.carlos@gmail.com>
2022-10-12 12:38:07 -0500

    added ReaderLink code

--- src/components/ReaderLink.js
+++ src/components/ReaderLink.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-
-const ReaderLink = () => {
+const ReaderLink = ({reader}) => {
   return (
-    <div> 
-            <h3>readerlink</h3>
-    
+    <div> <Link to={`/readers/${reader.id}`}>
+            <h3>{reader.name}</h3>
+        </Link>
     </div>
   )
 }
